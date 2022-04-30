eee89be9955249bb920ee90ca34eb504beed6940
CarlosJimeEnez <jjimenezpena7@gmail.com>
2022-04-30 03:00:08 -0500

    Fuzzy

--- src/main.cpp
+++ src/main.cpp
@@ -1,144 +1,137 @@
 #include <Arduino.h>
-#include <math.h>
 #include <map>
-#include<Plotter.h>
-
-using namespace std;
-
-#define trig D3 
-#define echo D8
-
-#define IN1 D0 
-#define IN2 D1 
-#define ENA1 D2
-
-double x; 
-
-float ad2volt(){
-  float adc_lect = float(analogRead(A0)); 
-  float v_in = (adc_lect*3.3)/1023; 
-  // float sum = 0; 
-  // for(int i = 0; i < 10; i++){
-  //   sum = v_in + sum; 
-  // }
-  // v_in = sum/10; 
-
-  return v_in;  
-}
-
-struct Motor{
-  void encender(){
-    digitalWrite(IN1, HIGH); 
-    digitalWrite(IN2, LOW);
-    analogWrite(PWM, 1023);  
-  }; 
-
-  int PWM;
-};
-
-struct Func_meb{
+#include <vector>
+#include<string>
+#include<map>
+using namespace std; 
+struct Func_meb {
   //Funciones: 
-  friend void build_graph(); 
-  void get_name(){
-    Serial.println(nombre); 
+  Func_meb(char *nombre, std::string tipo, double m, double k){
+    this ->k = k; 
+    this ->m = m; 
+    this->tipo = tipo; 
+    strcpy(this ->nombre, nombre); 
   }
+
+  void mostrar_valores(){
+    Serial.println(this->k);
+    Serial.println(this->m); 
+    Serial.println(this->nombre); 
+  }
+
+  //friend map<int, vector<double>> build_graph();
 
   //Atributos: 
-  char nombre[10] = " "; 
-  String tipo; 
-  float m = 0; 
-  float k = 0;  
+  char nombre[10] = " ";
+  std::string tipo = "";
+  double m = 0;
+  double k = 0;
+};
+//Clase para hacer arreglos de arreglos: 
+struct Arreglo_arreglos
+{
+    //Constructor: 
+    Arreglo_arreglos(vector<float> arreglo) {
+        for (auto i : arreglo) {
+            this->arreglo.push_back(i); 
+        }
+    }
+
+    //atributos: 
+    vector<float> arreglo; 
 };
 
-void build_graph(char *nombre, float rango, Func_meb *funciones_meb, int length_fun_memb){ 
-    int rango_grafica =  rango/0.01; 
-    Serial.println(rango_grafica); 
+//Calculo de los valroes del eje y: 
 
-    //Inicializa el valor del rango del eje Y: 
-    float y_values[rango_grafica]; 
-    float x_values[rango_grafica]; 
-
-    //Realiza un recorrido por todas las funciones de membresia y comprueba si es Gaussiana, luego 
-    //guarda el valor de cada elemento de esta funcion en un array: 
-    for (int i = 0; i < length_fun_memb; i++)
-    {    
-      //Si la funcion es del tipo exp: 
-        if (funciones_meb[i].tipo == "exp")
-        {
-          Serial.println("Exponencial");
-
-          //Construccion de la grafica: 
-          for (int j = 0; j < rango_grafica; j++){
-            y_values[j] = exp(-funciones_meb[i].k *(pow( x_values[j] - funciones_meb[i].m, 2)));
-            Serial.println(y_values[j]); 
+// std::map<int, std::vector<double>>
+std::map<int, vector<float>> build_graph(char* Input, vector<Func_meb> &funciones_memb, vector<float> &time, vector<float> &y_values){
+    std::map<int, vector<float>> values_to_return;  
+    
+    //Recorrido por todas las funciones de membr, toma sus atributos y calcula el valor de pertenencia de cada uno: 
+    for (int i = 0; i < funciones_memb.size(); i++) {
+        if (funciones_memb[i].tipo == "exp") {
+          Serial.println("EXP"); 
+          for (int j = 0; j < time.size(); j++) {
+              double cuadrado_x = pow(time[j] - funciones_memb[i].m, 2);
+              y_values[j] = exp(- funciones_memb[i].k * cuadrado_x); 
           }
-
-        }else{
-          Serial.println("IdontNow"); 
+          values_to_return.insert({ i, y_values});  
         }
-    Serial.println("Hi"); 
+        else{
+            Serial.println("No es exp"); 
+        }
     }
+    return values_to_return; 
 }
 
-Func_meb fun1 = {"Baja", "exp", 0.001, 1}; 
-Func_meb fun2 = {"Media", "exp", 3, 0.4}; 
-Func_meb fun3 = {"Optima", "exp", 6, 3.5}; 
-Func_meb fun4 = {"MediaAlta", "exp", 7.5, 2}; 
-Func_meb fun5 = {"Alta", "exp", 10, 0.9}; 
-Func_meb funciones_meb[] = {fun1, fun2, fun3, fun4, fun5};
 
-// Inicializa un objeto de la clase ventilador: 
-Motor ventilador = {ENA1}; 
-
-//Sharp
-float adc_lect = 0; 
-float v_in = 0; 
-
-//Ultrasonico: 
-int dist = 0; 
-float tiempo_enc = 0; 
-Plotter p; 
-/// ----------- Setup 
-void setup() {
-  Serial.begin(115200);
-  
-  //Tamaño del vector funciones_meb: 
-  int length_func_memb_array = 0;
-  for(Func_meb funciones: funciones_meb){
-    length_func_memb_array += 1; 
+///Inicio del vector del tiempo: 
+std::vector<float> tiempo(const float init_time, const float finish_time)
+{
+  //Inicio del vector x: Se les dan valores de 0 - 10 con espaciado de 0.001
+  std::vector<float> time;
+  int rango = finish_time - init_time; 
+  for (float i = 0; i < rango; i += 0.01) {
+      time.push_back(i);
   }
- 
-  //Inicia el objeto de la clase Grafica y prepara los valores para crear la grafica de 
-  //entrada 1. 
-  build_graph("Input", 10, funciones_meb, length_func_memb_array);
-   
-
-  pinMode(IN1, OUTPUT); 
-  pinMode(IN2, OUTPUT); 
-  pinMode(ventilador.PWM, OUTPUT);
-  pinMode(trig, OUTPUT); 
-  pinMode(echo, INPUT);
   
-  p.Begin(); 
- 
-  // //Auto: deduce el tipo de variable entrante: 
-  // for(auto nombre: funciones_meb){
-  //   nombre.get_name();
-  // }   
+  int32_t length = time.size();
+  int capacity = time.capacity();
+  return time;
 }
 
-void loop() {
-  
-  p.Plot(); // usually called within loop()
+///
+void setup()
+{
+  Serial.begin(115200);
+  //Inicializamos el eje y y x: 
+  vector<float> time = tiempo(2.5, 48.5); 
+  vector<float> y_values(time.size(), 0); 
 
-  // digitalWrite(trig, HIGH); 
-  // delay(10); 
-  // digitalWrite(trig,LOW); 
-  // tiempo_enc = pulseIn(echo, HIGH);
-  // dist = tiempo_enc /58.2 ; 
-  // Serial.println(dist);  
-  // delay(100); 
-  // // ventilador.encender();
-  // v_in = ad2volt(); 
-  // Serial.println(v_in);
+  // for (auto &&i : time)
+  // {
+  //   Serial.println(i); 
+  // }
+
+//  Funciones de membresia: 
+  Func_meb fun1("NB", "exp", 2.5, 0.03); 
+  Func_meb fun2("NM", "exp", 10.13, 0.04);
+  Func_meb fun3("NS", "exp", 19.71, 0.04);
+  Func_meb fun4("Z", "exp", 27, 0.09);
+  Func_meb fun5("PS", "exp", 33.6, 0.05);
+  Func_meb fun6("PM", "exp", 40.87, 0.04);
+  Func_meb fun7("PB", "exp", 48.5, 0.03);
+  Func_meb arreglo[] = { fun1, fun2, fun3, fun4, fun5, fun6, fun7};
+
+  std::vector<Func_meb> funciones_memb;
+  for (auto fun : arreglo) {
+      funciones_memb.push_back(fun);
+      fun.mostrar_valores(); 
+  }
+
+  //Inicia el objeto de la clase Grafica y prepara los valores para crear la grafica de 
+  //entrada 1.
+  char Input[10] = "Input1";
+  auto y_values_map = build_graph(Input, funciones_memb, time, y_values);
+
+  //Obtien los valores retornados en y_values_map y los organiza en y_values
+  vector<float> contendor_temp; 
+  vector<Arreglo_arreglos> y_values_arreglo; 
+  for (auto i = 0; i < funciones_memb.size(); i++) {
+    auto item = y_values_map.find(i);
+    
+    Serial.println(item->second[3]);
+    // for (float i : item->second){
+    //     contendor_temp.push_back(i); 
+    // }
+
+  //       // Arreglo_arreglos contenedor(contendor_temp);
+  //       // y_values_arreglo.push_back(contenedor);  
+  //       contendor_temp.clear(); 
+     }
+}
+
+void loop()
+{
+
 }
