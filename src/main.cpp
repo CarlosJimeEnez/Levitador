042b2fa9bab68ff6748dd6dce958b81b945c3a67
CarlosJimeEnez <jjimenezpena7@gmail.com>
2022-05-09 02:03:13 -0500

    paginaweb se sobrecarga la mem

--- src/main.cpp
+++ src/main.cpp
@@ -190,115 +190,155 @@ void setup() //////// Setup ////////////
 
   Serial.begin(9600);
   web_setup(); 
-//   Serial.println("Cargando funciones de membresia: "); 
-//   //Inicio del tiempo: 
-//   float init_time = 2.5; 
-//   float finish_time = 48.5;
 
-//   float init_time2 = k - finish_time; 
-//   float finish_time2 = k - init_time; 
+  Serial.println("Cargando funciones de membresia: "); 
+  //Input 1 vectores: 
+  float init_time = 2.5; 
+  float finish_time = 48.5;
 
-//   //Salida configuraciones: 
-//   float init_salida_val = 4; 
-//   float finish_salida_val = 10;
+  //Error configuraciones: 
+  float init_time2 = k - finish_time; 
+  float finish_time2 = k - init_time; 
 
-//   //Funcion para automatizar el desplieuge de las graficas: 
-//   vector<float> time = tiempo(init_time, finish_time , 0.01);
-//   vector<float> y_values(time.size(), 0);
+  //Salida configuraciones: 
+  float init_salida_val = 4; 
+  float finish_salida_val = 9;
   
-//   float rango_total = finish_salida_val - init_salida_val; 
-//   int num_fun = 7; 
-//   float Espacio_funciones = rango_total/num_fun; 
-//   vector<float> m; 
-//   m.push_back(init_salida_val);
-//   float init_salida = init_salida_val; 
+  //Velocidad configuraciones: 
+  float init_vel_time = -5; 
+  float finish_vel_time = 5;
   
-//   for (int i = 0; i < num_fun; i++)
-//   {
-//     init_salida = init_salida + Espacio_funciones; 
-//     m.push_back(init_salida); 
-//   }
+  //Funcion para automatizar el despliegue de funciones:
+  float rango_total = finish_salida_val - init_salida_val; 
+  int num_fun = 7; 
+  float espacio_entre_func = rango_total/num_fun; 
+  vector<float> m; //Valor donde se coloca la funcion de membresia. 
+  m.push_back(init_salida_val);
+  
+  float init_salida = init_salida_val; 
+  for (int i = 0; i < num_fun; i++)
+  {
+    init_salida = init_salida + espacio_entre_func; 
+    m.push_back(init_salida); 
+  }
+  
+//Funciones de membresia INPUT1: 
+  Func_meb fun1("NB", "exp", init_time, 0.03); 
+  Func_meb fun2("NM", "exp", 10.13, 0.04);
+  Func_meb fun3("NS", "exp", 17.51, 0.04);
+  Func_meb fun4("Z", "exp", k, 0.09);
+  Func_meb fun5("PS", "exp", 31.9, 0.05);
+  Func_meb fun6("PM", "exp", 30.85, 0.04);
+  Func_meb fun7("PB", "exp", finish_time, 0.03);
+  Func_meb arreglo[] = { fun1, fun2, fun3, fun4, fun5, fun6, fun7};
+  std::vector<Func_meb> funciones_memb;
+  for (auto fun : arreglo) {
+      funciones_memb.push_back(fun);
+      fun.mostrar_valores(); 
+  }
+
+  //ERROR func_membr: 
+  Func_meb fun8("NB", "exp", init_time2, 0.004);
+  Func_meb fun9("NS", "exp", finish_time2, 0.004);
+  Func_meb fun18("Z", "exp", 0, 0.1);
+  Func_meb error_arreglo[] = { fun8, fun9, fun18};
+  std::vector<Func_meb> error_func_membr;
+  for (auto fun : error_arreglo) {
+      error_func_membr.push_back(fun);
+      //fun.mostrar_valores(); 
+  }
+
+  //SALIDA funcion membre:  
+    Func_meb fun10("NB", "exp", m[0], 6);
+    Func_meb fun11("NM", "exp", m[1], 5);
+    Func_meb fun12("NS", "exp", m[2], 5);
+    Func_meb fun13("Z",  "exp", m[3], 5);
+    Func_meb fun14("PS", "exp", m[4], 5);
+    Func_meb fun15("PM", "exp", m[5], 5);
+    Func_meb fun16("PB", "exp", m[6], 5);
+    Func_meb salida_arreglo[] = { fun10, fun11, fun12, fun13, fun14, fun15, fun16 };
+    for (auto fun : salida_arreglo) {
+        salida_func_membr.push_back(fun);
+    }
+
+  //Hecho funciones membr: 
+  Func_meb fun17("Hecho", "exp", 25, 0.1);
+  Func_meb hecho_arreglo[] = {fun17};
+  std::vector<Func_meb> hecho_func_membr;
+  for (auto fun : hecho_arreglo) {
+      hecho_func_membr.push_back(fun);
+  }
+
+  //Velocidad funciones membr: 
+  Func_meb fun19("NS", "exp", init_vel_time, 0.09);
+  Func_meb fun20("Z", "exp", 0, 1);
+  Func_meb fun21("PS", "exp", finish_vel_time, 0.09);
+  Func_meb vel_arreglo[] = {fun18, fun19, fun20};
+  std::vector<Func_meb> vel_func_membr;
+  for (auto fun : vel_arreglo) {
+      vel_func_membr.push_back(fun);
+      //fun.mostrar_valores(); 
+  }
+
+  //Inicia el objeto de la clase Grafica y prepara los valores para crear la grafica de 
+  //entrada 1.
+  vector<float> time = tiempo(init_time, finish_time , 0.1);
+  vector<float> y_values(time.size(), 0);
+  char Input[10] = "Input1";
+  dist_values_map = build_graph(Input, funciones_memb, time, y_values);
+
+  ////Hecho: 
+  char Nombre_hecho[10] = "Hecho";
+  time.clear(); 
+  y_values.clear(); 
+  time = tiempo(init_time, finish_time , 0.1); 
+  for (int i = 0; i < time.size(); i++)
+  {
+      y_values.push_back(0); 
+  }
+  hecho_values_map = build_graph(Nombre_hecho, hecho_func_membr, time, y_values);
+
+  //Graph error 2:  
+  time.clear(); 
+  y_values.clear(); 
+  time = error_xval(init_time2, finish_time2);
+  for (int i = 0; i < time.size(); i++)
+  {
+      y_values.push_back(0); 
+  }
+  char Nombre[10] = "Error"; 
+  error_values_map = build_graph(Nombre, error_func_membr, time, y_values);
   
 
-// //Funciones de membresia INPUT1: 
-//   Func_meb fun1("NB", "exp", 2.5, 0.03); 
-//   Func_meb fun2("NM", "exp", 10.13, 0.04);
-//   Func_meb fun3("NS", "exp", 19.71, 0.04);
-//   Func_meb fun4("Z", "exp", 27, 0.09);
-//   Func_meb fun5("PS", "exp", 33.6, 0.05);
-//   Func_meb fun6("PM", "exp", 40.87, 0.04);
-//   Func_meb fun7("PB", "exp", 48.5, 0.03);
-//   Func_meb arreglo[] = { fun1, fun2, fun3, fun4, fun5, fun6, fun7};
+  /// Salida:
+  time.clear();
+  y_values.clear(); 
+  vector<float> time2 = error_xval(init_salida_val, finish_salida_val);
+  for (int i = 0; i < time.size(); i++)
+  {
+      y_values.push_back(0);
+  }
+  char Nombre_salida[10] = "salida";
+  salida_values_map = build_graph(Nombre_salida, salida_func_membr, time2, y_values);
 
-//   std::vector<Func_meb> funciones_memb;
-//   for (auto fun : arreglo) {
-//       funciones_memb.push_back(fun);
-//       fun.mostrar_valores(); 
-//   }
+  //Velocidad: 
+  time.clear(); 
+  y_values.clear(); 
+  time = tiempo(init_vel_time, finish_vel_time, 0.1);
+  for (int i = 0; i < time.size(); i++)
+  {
+      y_values.push_back(0);
+  }
+  char Nombre_velocidad[10] = "salida";
+  salida_values_map = build_graph(Nombre_velocidad, vel_func_membr, time, y_values);
 
-//   //ERROR func_membr: 
-//   Func_meb fun8("NB", "exp", -21.5, 0.004);
-//   Func_meb fun9("NS", "exp", 24.5, 0.004);
-//   Func_meb error_arreglo[] = { fun8, fun9 };
+  /// PWM resolucion, freq, canal setup. 
+  ledcSetup(canal0, freq, resolucion); 
+  ledcAttachPin(motor, canal0); 
 
-//   std::vector<Func_meb> error_func_membr;
-//   for (auto fun : error_arreglo) {
-//       error_func_membr.push_back(fun);
-//       //fun.mostrar_valores(); 
-//   }
-
-//   //SALIDA funcion membre:  
-//     Func_meb fun10("NB", "exp", m[0], 4);
-//     Func_meb fun11("NM", "exp", m[1], 4);
-//     Func_meb fun12("NS", "exp", m[2], 4);
-//     Func_meb fun13("Z", "exp", m[3], 4);
-//     Func_meb fun14("PS", "exp", m[4], 4);
-//     Func_meb fun15("PM", "exp", m[5], 4);
-//     Func_meb fun16("PB", "exp", m[6], 4);
-//     Func_meb salida_arreglo[] = { fun10, fun11, fun12, fun13, fun14, fun15, fun16 };
-    
-//     for (auto fun : salida_arreglo) {
-//         salida_func_membr.push_back(fun);
-//         //fun.mostrar_valores(); 
-//     }
-
-//   //Inicia el objeto de la clase Grafica y prepara los valores para crear la grafica de 
-//   //entrada 1.
-//   char Input[10] = "Input1";
-//   dist_values_map = build_graph(Input, funciones_memb, time, y_values);
-
-
-//   /////---- Graph error 2:  
-//   time.clear(); 
-//   y_values.clear(); 
-//   time = error_xval(init_time2, finish_time2);
-//   for (int i = 0; i < time.size(); i++)
-//   {
-//       y_values.push_back(0); 
-//   }
-//   char Nombre[10] = "Error"; 
-//   error_values_map = build_graph(Nombre, error_func_membr, time, y_values);
-  
-
-//   ////// -----  Salida:
-//   time.clear();
-//   y_values.clear(); 
-//   vector<float> time2 = error_xval(init_salida_val, finish_salida_val);
-//   for (int i = 0; i < time.size(); i++)
-//   {
-//       y_values.push_back(0);
-//   }
-
-//   char Nombre_salida[10] = "salida";
-//   salida_values_map = build_graph(Nombre_salida, salida_func_membr, time2, y_values);
-
-//   /// PWM resolucion, freq, canal setup. 
-//   ledcSetup(canal0, freq, resolucion); 
-//   ledcAttachPin(motor, canal0); 
-
-//   // Sensor_Ultrasonico: 
-//   pinMode(Trig, OUTPUT); 
-//   pinMode(Echo, INPUT); 
+  // Sensor_Ultrasonico: 
+  pinMode(Trig, OUTPUT); 
+  pinMode(Echo, INPUT); 
 }
 
 //////  --- LOOP --- //// ///////////// --------
@@ -307,73 +347,93 @@ void loop()
   webSocket.loop(); 
   server.handleClient(); 
 
-  // std::vector<float> fuzzy_val_dist; 
-  // std::vector<float> fuzzy_val_error;
-  // std::vector<float> kj; 
+  std::vector<float> fuzzy_val_dist; 
+  std::vector<float> fuzzy_val_error;
+  std::vector<float> fuzzy_val_hecho;
+  std::vector<float> kj; 
+  std::vector<float> alpha_j; 
 
-  // //INPUT: 
-  // float distancia = calc_dist(5);
-  // float error = k - distancia; 
+  //INPUT: 
+  float distancia = calc_dist(5);
+  float error = k - distancia; 
 
-  // //FUZZIFICAR la distancia:
-  // //Toma el par de valores en la posicion i y la asigna a item, para acceder a el array de valor de <y> 
-  // //se usa el el puntero item->second.
-  // fuzzy_val_dist = fuzzy_input(dist_values_map, distancia, 2.5);  
-  // fuzzy_val_error = fuzzy_input(error_values_map, error, -21.5); 
+  
+  // //Velocidad: 
+  // float vel = 0;
+  // int16_t time1 = millis();
+  // double pos1 = distancia * 10;  
+  // delay(50); 
+  // double pos2 = calc_dist(1) * 10; 
+  // int16_t time2 = millis(); 
+  
+  // vel =  (pos2 - pos1)/(time2 - time1); 
+  // Serial.print("VEL: -------------------------------> "); 
+  // Serial.println(vel);
+  // Serial.print("Time2: "); 
+  // Serial.println(time2);
 
-  // //Calculo de KJ:
-  // for(auto i: fuzzy_val_dist){
-  //   for(auto j: fuzzy_val_error){
-  //     kj.push_back(min(i,j));   
-  //   }
-  // } 
 
+  //FUZZIFICAR la distancia:
+  //Toma el par de valores en la posicion i y la asigna a item, para acceder a el array de valor de <y> 
+  //se usa el el puntero item->second.
+  fuzzy_val_dist = fuzzy_input(dist_values_map, distancia, 2.5, 100);  
+  fuzzy_val_error = fuzzy_input(error_values_map, error, -21.5, 100); 
+  fuzzy_val_hecho = fuzzy_input(hecho_values_map, distancia, 2.5, 10); 
 
+  //Calculo de KJ:
+  for(auto i: fuzzy_val_dist){
+    for(auto j: fuzzy_val_error){
+      float kj_local = min(i,j); 
+      kj.push_back(kj_local);   
+    }
+  } 
+
+  Serial.println("Here"); 
   // //Calculo del grado de consistencia 
-
-  // //Defuzzificacion:
-  // float num_defuzzy = 0; 
-  // float den_defuzzy = 0; 
-  // vector<float> mult_kj_cj;
-  // //Los centroides estan colocados de la forma: 
-  // // NB and NB -> NB, NB and PB -> NB 
-  // // NM and NB -> NM, NM and PB -> NM 
-  // // NS and NB -> NS, NS and PB -> NS 
-  // vector<float> cj = {
-  //   salida_func_membr[0].m , salida_func_membr[0].m,
-  //   salida_func_membr[0].m, salida_func_membr[0].m,
-  //   salida_func_membr[3].m, salida_func_membr[3].m,
-  //   salida_func_membr[3].m, salida_func_membr[3].m, 
-  //   salida_func_membr[4].m, salida_func_membr[4].m, 
-  //   salida_func_membr[5].m, salida_func_membr[5].m,
-  //   salida_func_membr[6].m, salida_func_membr[6].m
-  // }; 
-
-  // for (size_t i = 0; i < kj.size(); i++)
-  // {
-  //   float mult_num = kj[i] * cj[i]; 
-  //   mult_kj_cj.push_back(mult_num); 
-  // }
-  
-  // for(auto i: mult_kj_cj){
-  //   num_defuzzy += i; 
-  // }
-  
-  // //Denomiandor: 
   // for(auto i: kj){
-  //   den_defuzzy += i; 
+  //   alpha_j.push_back(max(min(fuzzy_val_hecho[0], i))); 
   // }
 
+  //Defuzzificacion:
+  float num_defuzzy = 0; 
+  float den_defuzzy = 0; 
+  vector<float> mult_kj_cj;
+  //Los centroides estan colocados de la forma: 
+  // NB and NB -> NB, NB and PB -> NB, NB and Z -> NB,  
+  // NM and NB -> NB, NM and PB -> NB , NM and Z -> NB
+  vector<float> cj = {
+    //ERROR: NB                     //NM                    //Z
+    salida_func_membr[0].m , salida_func_membr[0].m, salida_func_membr[0].m,
+    salida_func_membr[0].m, salida_func_membr[0].m,  salida_func_membr[0].m,
+    salida_func_membr[2].m, salida_func_membr[2].m,  salida_func_membr[0].m,
+    salida_func_membr[3].m, salida_func_membr[3].m,  salida_func_membr[3].m,
+    salida_func_membr[4].m, salida_func_membr[4].m,  salida_func_membr[4].m,
+    salida_func_membr[5].m, salida_func_membr[5].m, salida_func_membr[5].m,
+    salida_func_membr[6].m, salida_func_membr[6].m,  salida_func_membr[6].m,
+  }; 
 
-  // float v_out = num_defuzzy/den_defuzzy; 
-  // Serial.println("v_out"); 
-  // Serial.println(v_out); 
-  // //Conversion del volt salida en el duty cycle del pwm.  
-  // int duty_cycle = 83.697 * v_out + 62.775 ; 
+  for (size_t i = 0; i < kj.size(); i++)
+  {
+    float mult_num = kj[i] * cj[i]; 
+    mult_kj_cj.push_back(mult_num); 
+  }
+  
+  for(auto i: mult_kj_cj){
+    num_defuzzy += i; 
+  }
+  
+  //Denomiandor: 
+  for(auto i: kj){
+    den_defuzzy += i; 
+  }
 
-  // ledcWrite(canal0, duty_cycle); 
-  // Serial.println("duty_cycle"); 
-  // Serial.println(duty_cycle); 
+  float v_out = num_defuzzy/den_defuzzy; 
+ // Serial.println("v_out"); 
+ // Serial.println(v_out); 
+  //Conversion del volt salida en el duty cycle del pwm.  
+  int duty_cycle = 83.697 * v_out + 62.775 ; 
 
-  // float tiempo_fijo = millis(); 
+  ledcWrite(canal0, duty_cycle); 
+ // Serial.println("duty_cycle"); 
+ // Serial.println(duty_cycle); 
 }
