231233b3510c5db06a9df716189cc0732bdeae5a
CarlosJimeEnez <jjimenezpena7@gmail.com>
2022-05-10 09:30:29 -0500

    Graphs js

--- paginaWeb/static/js/grap.js
+++ paginaWeb/static/js/grap.js
@@ -1,3 +1,20 @@
-alert("Here")
 const socket = io()
 socket.emit("message", "hello") 
+TESTER = document.getElementById('chart');
+
+function random_vals(){
+	return Math.random()
+}
+
+Plotly.newPlot( TESTER, [{
+	y: [random_vals()],
+	type: "line"
+}]);
+
+setInterval(function(){
+	Plotly.extendtracesf
+}
+,200)	
+
+console.log(Morris.Line.data)    
+
