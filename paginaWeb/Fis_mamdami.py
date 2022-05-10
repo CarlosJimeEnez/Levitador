e03f4b42edacb2eaa973980e6a81dff0daf4b058
CarlosJimeEnez <jjimenezpena7@gmail.com>
2022-05-10 10:31:08 -0500

    Vectors en la pagina web init

---
+++ paginaWeb/Fis_mamdami.py
@@ -0,0 +1,56 @@
+from tracemalloc import start, stop
+import numpy as np
+class Fis: 
+    def __init__(self, **kw):
+        self.name = kw['name']
+        self.rango = kw['rango']
+        self.func_memb = kw['func_membr']
+        self.func_value = np.zeros(np.size(self.rango))
+        self.func_dicc_values = {}
+        self.belonging_value_return = []
+
+    def build_function(self):
+        '''Retorna un diccionario con los valores de cada grafica en distintas keys''' 
+        for funcion in self.func_memb: 
+            #Gaussiana:
+            if (funcion['tipo'] == 'exp'): 
+                k = funcion['k']
+                m = funcion['m']
+                rango = self.rango
+                value = self.func_value
+                for i in np.arange(start=0, stop=np.size(self.rango)):
+                    value[i] = np.exp(-k*(rango[i]-m)**2)
+
+                value = np.copy(value)
+                new_value = {funcion['nombre']:value}
+                self.func_dicc_values.update(new_value)
+            if (funcion['tipo'] == 'gauss'): 
+                b = funcion['b'] 
+                c = funcion['c']
+                rango = self.rango 
+                value =self.func_value
+                for i in np.arange(start=0, stop=np.size(self.rango)): 
+                    value[i] = 2*(np.exp(-(rango[i] - b)**2))/(2*c**2)
+                    
+                value = np.copy(value)
+                new_value = {funcion['nombre']:value}
+                self.func_dicc_values.update(new_value)
+
+        return self.func_dicc_values
+    
+    def belonging_value(self, input_value):
+        '''Retorna el valor de pertenencia a cada funcion de membresia de e el valor de entrada dado:'''
+        new_range = np.arange(start=0, stop=input_value, step=0.001)
+        belonging_value = np.zeros(np.size(new_range))        
+        for function in self.func_memb: 
+            #Gaussiana: 
+            if function['tipo'] == 'exp':
+                k = function['k']
+                m = function['m'] 
+                for i in np.arange(start= 0, stop=np.size(new_range)): 
+                    belonging_value[i] = np.exp(-k*(new_range[i]-m)**2)
+                
+                belonging_value = np.copy(belonging_value)
+                self.belonging_value_return.append(belonging_value[-1])  
+
+        return self.belonging_value_return        
--- paginaWeb/main.py
+++ paginaWeb/main.py
@@ -3,7 +3,8 @@ from flask import Flask, render_template
 from flask_mqtt import Mqtt
 from flask_socketio import SocketIO, send, emit
 from matplotlib.font_manager import json_load
-
+import numpy as np 
+from Fis_mamdami import Fis
 
 app = Flask(__name__)
 app.config['MQTT_BROKER_URL'] = 'broker.emqx.io'  # use the free broker from HIVEMQ
@@ -16,6 +17,32 @@ mqtt = Mqtt(app)
 socketio = SocketIO(app)
 
 print("Hello World from the begining")
+start = 2.5;  stop = 48.5
+rango = np.arange(start=start, stop=stop, step=0.001)
+rango_total = stop - start 
+num_fun = 7
+Espacio_funciones = rango_total/num_fun 
+b = []
+
+b.append(start)
+for i in range(0, num_fun): 
+    start = start + Espacio_funciones
+    print(start)
+    b.append(start)
+    
+#Funciones de mebresias: 
+func_memb = [
+    {'nombre': 'Baja', 'tipo': 'exp', 'm': b[0], 'k': 1},
+    {'nombre': 'Media', 'tipo': 'exp', 'm': b[1], 'k': 0.1}, 
+#     {'nombre': 'Optima', 'tipo': 'exp', 'm': b[2], 'k': Espacio_funciones},
+#     {'nombre': 'MediaAlta', 'tipo': 'exp', 'm': b[3], 'k': Espacio_funciones},
+#     {'nombre': 'Alta', 'tipo': 'exp', 'm': b[4], 'k': Espacio_funciones},
+#    {'nombre': 'SuperAlta', 'tipo': 'exp', 'm': b[5], 'k': Espacio_funciones},
+#     {'nombre': 'UltraAlta', 'tipo': 'exp', 'm': b[6], 'k': Espacio_funciones},
+]
+
+object = Fis(name = "Input 1", rango = rango, func_membr = func_memb)
+humedad_functions = object.build_function()
 
 
 @app.route('/')
